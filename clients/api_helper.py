05712fa6ceb3332f48e07d9365d7de31fc83be66
Timur.Sharafutdinov <Timur.Sharafutdinov@south.rt.ru>
2021-12-25 16:41:36 +0300

    add initialize and finalize in base fixture

--- clients/api_helper.py
+++ clients/api_helper.py
@@ -6,20 +6,36 @@ class ApiPetStore:
 
     def __init__(self):
         self.url = Config.url
-        self.default_headers = {"accept": "application/json"}
+        self.headers = {'accept': 'application/json'}
+
+    def create_user(self, data):
+        url = self.url + '/user'
+        response = requests.post(url, headers=self.headers, json=data)
+        return response
+
+    def delete_user(self, username):
+        url = self.url + f'/user/{username}'
+        response = requests.delete(url, headers=self.headers)
+        return response
+
+    def login(self, username, password):
+        url = self.url + f'/user/login'
+        params = {'username': username, 'password': password}
+        response = requests.get(url, headers=self.headers, params=params)
+        return response
+
+    def logout(self, session_id):
+        url = self.url + f'/user/logout'
+        data = {'session': session_id}
+        response = requests.get(url, headers=self.headers, json=data)
+        return response
 
     def create_pet(self, data):
-        method = '/pet'
-        url = self.url + method
-
-        response = requests.post(url, headers=self.default_headers, json=data)
-
+        url = self.url + '/pet'
+        response = requests.post(url, headers=self.headers, json=data)
         return response
 
     def get_pet_by_id(self, pet_id):
-        method = f'/pet/{pet_id}'
-        url = self.url + method
-
-        response = requests.get(url, headers=self.default_headers)
-
+        url = self.url + f'/pet/{pet_id}'
+        response = requests.get(url, headers=self.headers)
         return response
--- conftest.py
+++ conftest.py
@@ -4,12 +4,31 @@ from random import choice
 from application import Application
 
 
-fixture = Application()
-
-
 @pytest.fixture(scope="session")
 def base_fixture():
-    return fixture
+    """
+    Инициализация:
+    1. Создаем юзера
+    2. Логинимся под созданным юзером
+    3. Прокидываем сессию в заголовки
+
+    Финализация:
+    1. логаут
+    2. удаляем юзера
+    """
+    fixture = Application()
+    username = ''.join(choice(string.ascii_lowercase) for i in range(7))
+    password = ''.join(choice(string.ascii_lowercase) for i in range(7))
+    user_data = {'username': username, 'password': password}
+    fixture.api.create_user(user_data)
+    login_response = fixture.api.login(username, password)
+    session_id = login_response.json()['message'][23:]
+    fixture.api.headers.update({'session': session_id})
+
+    yield fixture
+
+    fixture.api.logout(session_id)
+    fixture.api.delete_user(username)
 
 
 @pytest.fixture()
@@ -18,8 +37,8 @@ def pet_id():
     name = ''.join(choice(string.ascii_lowercase) for i in range(5))
     category = ''.join(choice(string.ascii_lowercase) for i in range(7))
     status = choice(['available', 'pending', 'sold'])
-    data = fixture.helper.gen_create_pet_req(name, category, status)
+    data = base_fixture.helper.gen_create_pet_req(name, category, status)
 
-    response = fixture.api.create_pet(data)
+    response = base_fixture.api.create_pet(data)
 
     return response.json()['id']
